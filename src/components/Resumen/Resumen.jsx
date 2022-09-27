b2fa219bb311e738d8b3a8c8ceedb6787e306b9e
unknown <fabian179rc@gmail.com>
2022-09-27 15:39:01 -0300

    add download CV

--- src/components/Resumen/Resumen.jsx
+++ src/components/Resumen/Resumen.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import logo from "../../img/logo3.png";
+import Cv from "../../Cv/Fabian-Santos-Fullstack-Developer.pdf";
 import s from "./Resumen.module.css";
 import { MdOutlineCloudDownload } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 export default function Resumen() {
   return (
     <div className={s.navbar}>
       <img src={logo} alt="Logo" className={s.logo} />
-      <div className={s.button}>
-        <span>RESUMEN </span>
-        <a href="https://github.com/fabian179rc"></a>
-        <MdOutlineCloudDownload
-          className={`${s.btnSocialMedia} ${s.simbol}`}
-          size="2em"
-          title="Github"
-        />
-      </div>
+      <Link
+        className={s.link}
+        href={Cv}
+        target="_blank"
+        rel="noopener noreferrer"
+        download="Fabian-Santos-Fullstack-Developer.pdf"
+      >
+        <div className={s.button}>
+          <span>RESUMEN </span>
+
+          <MdOutlineCloudDownload
+            className={`${s.btnSocialMedia} ${s.simbol}`}
+            size="2em"
+            title="Github"
+          />
+        </div>
+      </Link>
     </div>
   );
 }
