1da50bfb5c0337550ddf709776b7a6a18cb8f9f8
unknown <fabian179rc@gmail.com>
2022-09-29 12:39:52 -0300

    fixed position responsive navbar

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,23 @@
-import { Link } from "react-router-dom";
+import s from "./About.module.css";
+import picture from "../../img/foto.png";
 export default function About() {
   return (
-    <>
-      <div>Hola! Soy Fabián, un Developer </div>
-    </>
+    <div className={s.container}>
+      <div className={s.about}>
+        <b className={s.b1}>
+          Soy Fabian, un Desarrollador enfocado por el buen funcionamiento del
+          codigo limpio, siempre buscando mejorar mis buenas prácticas. Me
+          motiva trabajar en equipos donde la diversidad de opiniones, aportan
+          al desarrollo grupal y hacer con esto, un excelente clima laboral.
+        </b>
+        <b className={s.b2}>Tecnologias: </b>
+        <b className={s.b3}>
+          React - Redux - Node - Express - Sequelize - JavaScript - HTML - CSS
+        </b>
+      </div>
+      <div className={s.foto}>
+        <img src={picture} alt="Picture" />
+      </div>
+    </div>
   );
 }
