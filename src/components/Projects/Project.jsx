1c60503b5eb31670e47f72fa7c3ade728c20f124
unknown <fabian179rc@gmail.com>
2022-10-07 15:52:18 -0300

    video changed hosting

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -78,17 +78,19 @@ function Proyect() {
       >
         <iframe
           className={s.video}
-          src="https://www.youtube.com/embed/LfSbr_aBc08"
+          src="https://player.vimeo.com/video/757962168?h=2b8de3953d&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
           frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allow="autoplay; fullscreen; picture-in-picture"
           allowFullScreen
+          title="StarCards musica.mp4"
         ></iframe>
         <iframe
           className={s.video}
-          src="https://www.youtube.com/embed/JN0hART65Ek"
+          src="https://player.vimeo.com/video/757963944?h=2313da659a&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
           frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allow="autoplay; fullscreen; picture-in-picture"
           allowFullScreen
+          title="VideoGames"
         ></iframe>
       </ReactSimplyCarousel>
     </>
